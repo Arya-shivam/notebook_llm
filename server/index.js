48f617dcc3dcd482a48ec828f59a127dd971d83e
arya_shivam <aryashivam54@gmail.com>
2025-08-30 22:35:11 +0000

    deoloy

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use("/upload",uploadRoute);
 app.use("/chat",chatRoute)
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
